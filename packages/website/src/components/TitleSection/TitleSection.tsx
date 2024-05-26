62f61cf8a780780fad57f657c387201e99954740
ethan-heo <ethan.hb94@gmail.com>
2024-05-26 21:18:03 +0900

    style: TitleSection의 description, section 너비를 수정한다

--- packages/website/src/components/TitleSection/TitleSection.tsx
+++ packages/website/src/components/TitleSection/TitleSection.tsx
@@ -2,10 +2,13 @@ import { Flex, Paragraph, Text, Title } from "@ethanheo/ui";
 import React from "react";
 import "./TitleSection.styles.css";
 import pkg from "../../../../generate-design-token/package.json";
+import useMediaSection from "../../hooks/useMediaSection";
 
 const TitleSection = () => {
+	const section = useMediaSection();
+
 	return (
-		<Flex className="title-section section" align="center" vertical>
+		<Flex className={`title-section ${section}`} align="center" vertical>
 			<Title className="title-section__title" variant="accent" strong>
 				Generate Design Token
 			</Title>
@@ -15,7 +18,7 @@ const TitleSection = () => {
 				</Text>
 			</Flex>
 			<Flex justify="center">
-				<Paragraph className="title-section__descripton" variant="accent">
+				<Paragraph className="title-section__description" variant="accent">
 					generate design token 라이브러리는 JSON 형식으로 디자인 토큰을 정의할
 					때 편의성을 제공하는 라이브러리입니다.
 				</Paragraph>
