ee7ae4aa047076fecbc45fdba338bcb6f903e6c0
ethan-heo <ethan.hb94@gmail.com>
2024-10-05 20:32:40 +0900

    feat: usecase 인터페이스를 정의한다

---
+++ packages/generate-design-token/src/types/usecase.ts
@@ -0,0 +1,21 @@
+import TokenProcessor from "../TokenProcessor";
+import { TokenIterator } from "./token";
+
+export interface Usecase {
+	readonly baseTokens: TokenProcessor[];
+
+	/**
+	 *
+	 * @param tokenIteratorItem
+	 * @description 입력된 토큰 이터레이터 아이템이 use case에 유효한지 검사한다
+	 * @returns
+	 */
+	validate(tokenIteratorItem: TokenIterator[number]): boolean;
+	/**
+	 *
+	 * @param tokenIteratorItem
+	 * @description 베이스 토큰을 참조하여 입력된 토큰 이터레이터 아이템을 변환한다
+	 * @returns
+	 */
+	transform(tokenIteratorItem: TokenIterator[number]): TokenIterator[number];
+}
