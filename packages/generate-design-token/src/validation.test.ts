c14b9f897e61f6596c3cf29e19c07e77bb9b71b0
ethan-heo <ethan.hb94@gmail.com>
2024-10-25 17:10:32 +0900

    feat: 토큰을 속성값을 검사하는 함수를 구현한다

--- packages/generate-design-token/src/validation.test.ts
+++ packages/generate-design-token/src/validation.test.ts
@@ -1,14 +1,16 @@
 import { expect, it } from "vitest";
 import {
-	validateOptionalTokenProperties,
-	validateRequiredTokenProperties,
+	hasDollarPrefix,
+	hasNotDollarPrefix,
+	hasNotTokenObjOrNotTokenStructure,
+	hasRequiredProp,
 } from "./validation";
 
 it.each([
 	[{}, false],
 	[{ $type: "color", $value: "#ff0000" }, true],
 ])(`토큰 객체의 필수 속성을 검사한다`, (tokenObj, expected) => {
-	expect(validateRequiredTokenProperties(tokenObj)).toBe(expected);
+	expect(hasRequiredProp(tokenObj)).toBe(expected);
 });
 
 it.each([
@@ -26,6 +28,99 @@ it.each([
 		},
 		false,
 	],
-])(`토큰 객체의 필수 속성을 검사한다`, (tokenObj, expected) => {
-	expect(validateOptionalTokenProperties(tokenObj)).toBe(expected);
-});
+])(
+	`객체 속성에 $가 prefix로 설정되어 있는지 확인한다`,
+	(tokenObj, expected) => {
+		expect(hasDollarPrefix(tokenObj)).toBe(expected);
+	},
+);
+
+it.each([
+	[
+		{
+			primary: {
+				$type: "color",
+				$value: "#ff0000",
+			},
+			secondary: {
+				$type: "color",
+				$value: "#00ff00",
+			},
+		},
+		true,
+	],
+	// [
+	// 	{
+	// 		$primary: {
+	// 			$type: "color",
+	// 			$value: "#ff0000",
+	// 		},
+	// 		$secondary: {
+	// 			$type: "color",
+	// 			$value: "#00ff00",
+	// 		},
+	// 	},
+	// 	false,
+	// ],
+])(
+	`객체 속성에 $가 prefix로 설정되어 있지 않은지 확인한다`,
+	(tokenObj, expected) => {
+		expect(hasNotDollarPrefix(tokenObj)).toBe(expected);
+	},
+);
+
+it.each([
+	[
+		{
+			primary: {
+				$type: "color",
+				$value: "#ff0000",
+			},
+			secondary: {
+				$type: "color",
+				$value: "#00ff00",
+			},
+		},
+		true,
+	],
+	[
+		{
+			$primary: {
+				$type: "color",
+				$value: "#ff0000",
+			},
+			$secondary: {
+				$type: "color",
+				$value: "#00ff00",
+			},
+		},
+		false,
+	],
+	[
+		{
+			primary: {
+				$type: "color",
+				$value: "#ff0000",
+			},
+			$value: "#00ff00",
+		},
+		false,
+	],
+	[
+		{
+			primary: {
+				$type: "color",
+				$value: "#ff0000",
+			},
+			$type: "color",
+			$value: "#00ff00",
+		},
+		false,
+	],
+	["test", false],
+])(
+	`토큰 구조 객체의 속성값이 토큰 객체 또는 토큰 구조 객체로 정의되어 있는지 검사한다.`,
+	(tokenObj, expected) => {
+		expect(hasNotTokenObjOrNotTokenStructure(tokenObj)).toBe(expected);
+	},
+);
--- packages/generate-design-token/src/validation.ts
+++ packages/generate-design-token/src/validation.ts
@@ -1,16 +1,50 @@
-export const validateRequiredTokenProperties = (value: {}): boolean => {
+export const hasRequiredProp = (value: {}): boolean => {
 	const MUST_HAVE_PROPERTIES = ["$type", "$value"];
 
 	return MUST_HAVE_PROPERTIES.every((prop) => prop in value);
 };
 
-export const validateOptionalTokenProperties = (value: {}): boolean => {
+/**
+ * @description 객체 속성 모두 이름에 $를 prefix로 가지고 있는지 확인한다
+ * @returns
+ */
+export const hasDollarPrefix = (value: {}): boolean => {
 	for (const prop in value) {
-		const has$ = prop.startsWith("$");
-
-		if (!has$) {
+		if (!prop.startsWith("$")) {
 			return false;
 		}
+	}
+
+	return true;
+};
+
+/**
+ * @description 객체 속성 모두 이름에 $를 prefix로 가지고 있지 않은지 확인한다
+ * @returns
+ */
+export const hasNotDollarPrefix = (value: {}): boolean => {
+	for (const prop in value) {
+		if (prop.startsWith("$")) {
+			return false;
+		}
+	}
+
+	return true;
+};
+
+export const hasNotTokenObjOrNotTokenStructure = (value: {}): boolean => {
+	if (typeof value === "object") {
+		// 토큰 객체인지 검사
+		if (hasRequiredProp(value) && !hasDollarPrefix(value)) {
+			return false;
+		}
+
+		// 토큰 구조 객체인지 검사
+		if (!hasRequiredProp(value) && !hasNotDollarPrefix(value)) {
+			return false;
+		}
+	} else {
+		return false;
 	}
 
 	return true;
