515ba4e60c56561e46ff0a7213de6d3614ee6b71
ethan-heo <ethan.hb94@gmail.com>
2024-10-25 14:31:43 +0900

    refactor: 토큰 순회 로직을 분리한다

--- packages/generate-design-token/src/Token.ts
+++ packages/generate-design-token/src/Token.ts
@@ -1,60 +1,53 @@
 import * as Types from "./TokenGenerator.types";
 import { validateRequiredTokenProperties } from "./validation";
 
-type Iteratee = (tokenRef: string, tokenValue: Types.Token) => boolean;
+type Iteratee = (tokenName: string, tokenValue: Types.Token) => boolean;
 
 class Token {
 	#token: Types.Token;
+
 	constructor(token: Types.Token) {
 		// 유효성 검사
 		this.#token = token;
 	}
 
 	find(callback: Iteratee): Types.Token | undefined {
-		const token = this.#token;
-		let stack: [string, Types.Token][][] = [Object.entries(token)];
-		let currentCtx: [string, Types.Token][] = stack[0];
 		let result: Types.Token | undefined;
 
-		while (stack.length) {
-			const [tokenName, tokenValue] = currentCtx.shift()!;
-
+		this.#iterator((tokenName, tokenValue) => {
 			if (callback(tokenName, tokenValue)) {
 				result = tokenValue;
-				break;
 			}
-
-			if (currentCtx.length === 0) {
-				stack.shift();
-				currentCtx = stack[stack.length - 1];
-			}
-
-			if (!this.#isTokenObj(tokenValue)) {
-				const item = Object.entries(tokenValue);
-				stack = [item, ...stack];
-				currentCtx = item;
-			}
-		}
+		});
 
 		return result;
 	}
 
 	findAll(callback: Iteratee): Types.Token[] {
+		const result: Types.Token[] = [];
+
+		this.#iterator((tokenName, tokenValue) => {
+			if (callback(tokenName, tokenValue)) {
+				result.push(tokenValue);
+			}
+		});
+
+		return result;
+	}
+
+	#iterator(callback: (tokenName: string, tokenValue: Types.Token) => void) {
 		const token = this.#token;
 		let stack: [string, Types.Token][][] = [Object.entries(token)];
 		let currentCtx: [string, Types.Token][] = stack[0];
-		let result: Types.Token[] = [];
 
 		while (stack.length) {
 			const [tokenName, tokenValue] = currentCtx.shift()!;
 
-			if (callback(tokenName, tokenValue)) {
-				result.push(tokenValue);
-			}
+			callback(tokenName, tokenValue);
 
 			if (currentCtx.length === 0) {
 				stack.shift();
-				currentCtx = stack[stack.length - 1];
+				currentCtx = stack[0];
 			}
 
 			if (!this.#isTokenObj(tokenValue)) {
@@ -63,8 +56,6 @@ class Token {
 				currentCtx = item;
 			}
 		}
-
-		return result;
 	}
 
 	#isTokenObj(token: Types.Token): token is Types.TokenObj {
