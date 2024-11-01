60f30ab14a5120b2ebdc0e3cce104d9f32df846e
ethan-heo <ethan.hb94@gmail.com>
2024-11-01 13:57:28 +0900

    feat: 이터레이터 순회를 전위 순회 방식으로 처리되도록 변경한다

--- packages/generate-design-token/src/Token.test.ts
+++ packages/generate-design-token/src/Token.test.ts
@@ -39,41 +39,22 @@ it(`[Token.find] 특정 조건에 맞는 토큰을 찾아 반환한다.`, () => 
 	const token = new Token(TOKEN);
 
 	expect(
-		token.find((tokenName) => {
-			return tokenName === "1";
+		token.find((props) => {
+			return props.join('.') === "color.tertiary.1";
 		}),
-	).toStrictEqual(["1", TOKEN.color.tertiary[1]]);
-
-	expect(token.find("color.primary")).toStrictEqual([
-		"primary",
-		TOKEN.color.primary,
-	]);
-	expect(token.find("color.primary.1")).toStrictEqual(undefined);
+	).toStrictEqual([["color", "tertiary", "1"], TOKEN.color.tertiary[1]]);
 });
 
 it(`[Token.findAll] 특정 조건에 맞는 토큰을 모두 찾아 반환한다.`, () => {
 	const token = new Token(TOKEN);
-	const actual1 = token.findAll((tokenName) => {
-		return tokenName === "primary" || tokenName === "secondary";
-	});
-	const expected1: [string, Types.Token][] = [
-		["primary", TOKEN.color.primary],
-		["secondary", TOKEN.color.secondary],
-	];
 
-	expect(
-		actual1.every(([name]) => expected1.some(([_name]) => name === _name)),
-	).toBeTruthy();
-
-	const actual2 = token.findAll(/(primary)|(secondary)/);
-	const expected2: [string, Types.Token][] = [
-		["primary", TOKEN.color.primary],
-		["secondary", TOKEN.color.secondary],
-	];
-
-	expect(
-		actual2.every(([name]) => expected2.some(([_name]) => name === _name)),
-	).toBeTruthy();
+	expect(token.findAll((props) => {
+		const tokenRef = props.join('.')
+		return tokenRef === "color.primary" || tokenRef === "color.secondary";
+	})).toStrictEqual([
+		[["color", "secondary"], TOKEN.color.secondary],
+		[["color", "primary"], TOKEN.color.primary],
+	]);
 });
 
 it(`토큰 유효성 검사를 진행한다.`, () => {
@@ -108,15 +89,4 @@ it(`토큰 유효성 검사를 진행한다.`, () => {
 			},
 		});
 	}).not.toThrowError();
-});
-
-it(`[Token.has] 입력된 값에 해당하는 토큰이 존재하는지 확인한다.`, () => {
-	const token = new Token(TOKEN);
-	const actual1 = "color.primary";
-	const expected1 = true;
-	const actual2 = (name) => name === "primary";
-	const expected2 = true;
-
-	expect(token.has(actual1)).toBe(expected1);
-	expect(token.has(actual2)).toBe(expected2);
 });
--- packages/generate-design-token/src/Token.ts
+++ packages/generate-design-token/src/Token.ts
@@ -7,8 +7,8 @@ import {
 import isTokenObj from "./isTokenObj";
 
 type Iteratee = (
-	tokenName: string,
-	tokenValue: Types.Token,
+	props: string[],
+	token: Types.Token,
 	self: Token,
 ) => boolean;
 
@@ -26,79 +26,82 @@ class Token {
 	 * @description 주어진 참조값에 해당하는 토큰 객체 및 구조 객체를 반환한다.
 	 * @returns
 	 */
-	find(arg: string): [string, Types.Token] | undefined;
-	find(arg: Iteratee): [string, Types.Token] | undefined;
-	find(arg: string | Iteratee): [string, Types.Token] | undefined {
-		if (typeof arg === "string") {
-			return this.#accessByPath(arg);
-		}
+	find(callback: Iteratee): Types.TokenResult | undefined {
+		let result: Types.TokenResult | undefined;
 
-		if (typeof arg === "function") {
-			const callback = arg as Iteratee;
-			let result: [string, Types.Token] | undefined;
-
-			this.#iterator(this.#token, (name, token) => {
-				if (callback(name, token, this)) {
-					result = [name, token];
-				}
-			});
-
-			return result;
-		}
-	}
-
-	/**
-	 * @description 주어진 참조값에 해당하는 모든 토큰 객체 및 구조 객체를 반환한다.
-	 * @returns
-	 */
-	findAll(arg: RegExp): [string, Types.Token][];
-	findAll(arg: Iteratee): [string, Types.Token][];
-	findAll(arg: RegExp | Iteratee): [string, Types.Token][] {
-		const result: [string, Types.Token][] = [];
-
-		this.#iterator(this.#token, (name, token) => {
-			if (arg instanceof RegExp) {
-				arg.test(name) && result.push([name, token]);
-			} else {
-				arg(name, token, this) && result.push([name, token]);
+		this.#iterator(this.#token, (props, token) => {
+			if (callback(props, token, this)) {
+				result = [props, token];
 			}
 		});
 
 		return result;
 	}
 
-	has(arg: string): boolean;
-	has(arg: Iteratee): boolean;
-	has(arg: string | Iteratee): boolean {
-		return !!this.find(arg as any);
+	/**
+	 * @description 주어진 참조값에 해당하는 모든 토큰 객체 및 구조 객체를 반환한다.
+	 * @returns
+	 */
+	findAll(callback: Iteratee): Types.TokenResult[] {
+		const result: Types.TokenResult[] = [];
+
+		this.#iterator(this.#token, (props, token) => {
+			callback(props, token, this) && result.push([props, token]);
+		});
+
+		return result;
 	}
 
-	replace(tokenName: string) {
-		// 1. 토큰 이름 경로를 구한다. ex) a.b.c
-		// 2. 구한 경로의 부모 토큰을 찾는다.
-		// 3. 부모 토큰에서 tokenName의 속성을 삭제하고 변경된 토큰을 추가한다
+	delete(tokenRef: string) {
+		const props = this.#transformPropsTo(tokenRef);
+
+		
+	}
+
+	add(tokenRef: string, token: Types.Token) {
+		const props = tokenRef.split(".");
+		const newProp = props.pop()!;
+		let temp = this.#token;
+
+		for (const prop of props) {
+			if (!temp[prop]) {
+				temp[prop] = {};
+			}	
+
+			temp = temp[prop] as Types.Token;
+		}
+
+		temp[newProp] = token;
 	}
 
 	#iterator(
 		token: Types.Token,
-		callback: (name: string, token: Types.Token) => void,
+		callback: (props: string[], token: Types.Token) => void,
 	) {
 		let stack: [string, Types.Token][][] = [Object.entries(token)];
-		let currentCtx: [string, Types.Token][] = stack.pop()!;
+		let currentCtx: [string, Types.Token][] = stack[stack.length - 1]!;
+		let props: string[] = []
 
 		while (currentCtx.length) {
-			const [name, token] = currentCtx.pop()!;
+			const [prop, token] = currentCtx.pop()!;
 
-			callback(name, token);
+			props.push(prop)
+			
+			callback(props, token)
 
-			// 1. 토큰 구조 객체인 경우 Array<[속성 이름, 토큰]> 형태로 변경하여 stack에 추가한다
 			if (!isTokenObj(token)) {
-				stack.push(Object.entries(token));
+				const item = Object.entries(token);
+
+				stack.push(item);
+				currentCtx = item
+			} else {
+				props = props.slice(0, stack.length - 1)
 			}
 
-			// 2. 컨텍스트(객체, 토큰)의 요소가 없으면 컨텍스트를 변경한다.
 			if (currentCtx.length === 0) {
-				currentCtx = stack.pop() ?? [];
+				stack.pop()
+				currentCtx = stack[stack.length - 1] ?? []
+				props = props.slice(0, stack.length - 1)
 			}
 		}
 	}
@@ -121,19 +124,8 @@ class Token {
 		});
 	}
 
-	#accessByPath(tokenRef: string): [string, Types.Token] | undefined {
-		const paths = tokenRef.split(".");
-		let token: Types.Token = this.#token;
-		let name: string = "";
-
-		for (const path of paths) {
-			token = token[path];
-			name = path;
-
-			if (!token) return;
-		}
-
-		return [name, token];
+	#transformPropsTo(tokenRef: Types.TokenRef) {
+		return tokenRef.split(".");
 	}
 }
 
