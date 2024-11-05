58e071654bc62db8a0d107edffe2b1766c894fc1
ethan-heo <ethan.hb94@gmail.com>
2024-11-05 18:21:20 +0900

    feat: 유즈케이스 핸들러 클래스에 케이스를 추가한다

--- packages/generate-design-token/src/useCases/index.ts
+++ packages/generate-design-token/src/useCases/index.ts
@@ -1,11 +1,16 @@
 import Token from "../Token";
 import UseCase from "./UseCase.abstract";
 import UseCase1 from "./UseCase1";
+import UseCase2 from "./UseCase2";
+import UseCase3 from "./UseCase3";
+import UseCase4 from "./UseCase4";
 
 class UseCases {
-	#useCases: UseCase[];
+	#useCases: UseCase<any>[];
 	constructor() {
-		this.#useCases = [UseCase1].map((UseCase) => new UseCase());
+		this.#useCases = [UseCase1, UseCase2, UseCase3, UseCase4].map(
+			(UseCase) => new UseCase(),
+		);
 	}
 
 	transform(baseToken: Token, tokens: Token[]) {
