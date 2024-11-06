e4a2cebd8feee2cbc29d5eda3c7b6751dd96221f
ethan-heo <ethan.hb94@gmail.com>
2024-11-06 15:26:36 +0900

    chore: declaration 파일을 생설하도록 빌드 옵션을 변경한다

--- packages/generate-design-token/configs/rollup.config.ts
+++ packages/generate-design-token/configs/rollup.config.ts
@@ -1,25 +1,32 @@
 import tsPlugin from "@rollup/plugin-typescript";
+import { dts } from "rollup-plugin-dts";
 
 const INPUT_PATH = "./src";
 const OUTPUT_PATH = "./dist";
 
-export default {
-	input: INPUT_PATH + "/index.ts",
-	output: [
-    {
-      file: OUTPUT_PATH + "/index.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: OUTPUT_PATH + "/index.es.js",
-      format: "es",
-      sourcemap: true,
-    }
-  ],
-	plugins: [
-		tsPlugin({
-      
-    }),
-	],
-};
+export default [
+	{
+		input: INPUT_PATH + "/index.ts",
+		output: [
+			{
+				file: OUTPUT_PATH + "/index.js",
+				format: "cjs",
+				sourcemap: true,
+			},
+			{
+				file: OUTPUT_PATH + "/index.es.js",
+				format: "es",
+				sourcemap: true,
+			},
+		],
+		plugins: [tsPlugin({})],
+	},
+	{
+		input: INPUT_PATH + "/index.ts",
+		output: {
+			file: OUTPUT_PATH + "/index.d.ts",
+			format: "es",
+		},
+		plugins: [dts()],
+	},
+];
