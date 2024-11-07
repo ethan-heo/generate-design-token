93d77c8b014780dbbdf386a4b4d8986895500c41
ethan-heo <ethan.hb94@gmail.com>
2024-11-07 22:47:22 +0900

    chore: website 패키지를 삭제한다

--- packages/website/src/hooks/useMediaSection.ts
+++
@@ -1,9 +0,0 @@
-import { useMediaQuery } from "@ethanheo/ui";
-
-const useMediaSection = () => {
-	const platform = useMediaQuery();
-
-	return `section--width-${platform}`;
-};
-
-export default useMediaSection;
