9a1d49df3c8b471579f5383bfedf63df591fda38
ethan-heo <ethan.hb94@gmail.com>
2024-11-20 00:54:39 +0900

    refactor: utils 코드를 외부에서 사용할 수 있도록 내보내기한다

--- packages/generate-design-token/src/index.ts
+++ packages/generate-design-token/src/index.ts
@@ -1,5 +1,5 @@
 export * as GDTTypes from "./types/token-objs.types";
-
 export { default as Token } from "./token";
-
 export { default as generateDesignToken } from "./generate-design-token";
+export { default as UseCases } from "./use-cases";
+export * from "./utils";
