acde9789b7f82a2dd0b8bdd1dfcb190eeaf33e3e
ethan-heo <ethan.hb94@gmail.com>
2024-11-24 22:11:17 +0900

    chore: 타입 내보내기를 변경한다

--- packages/generate-design-token/src/index.ts
+++ packages/generate-design-token/src/index.ts
@@ -1,4 +1,4 @@
-export * as GDTTypes from "./types/token-objs.types";
+export * from "./types";
 export { default as Token } from "./token";
 export { default as generateDesignToken } from "./generate-design-token";
 export * from "./utils";
